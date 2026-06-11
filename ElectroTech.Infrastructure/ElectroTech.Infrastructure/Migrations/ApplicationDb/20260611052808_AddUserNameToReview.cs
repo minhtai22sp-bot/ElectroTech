f9a16144cc59f5778fc1111d0c6745d6a7fdcf00
Mintai2806 <trinhminhtai0612@gmail.com>
2026-06-11 12:46:00 +0700

    view

---
+++ ElectroTech.Infrastructure/ElectroTech.Infrastructure/Migrations/ApplicationDb/20260611052808_AddUserNameToReview.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectroTech.Infrastructure.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class AddUserNameToReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Reviews",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Reviews");
+        }
+    }
+}
